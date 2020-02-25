6dce61397e9530866f09aa962d158313e2b2549a
GandyA23 <avila.gandy@gmail.com>
2020-02-25 01:28:40 -0600

    HomeController Modificado, modelos añadidos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 use App\Miembro;
+use App\Banner;
+use App\Causa;
+use App\Evento;
+use App\Noticia;
+use App\Proyecto;
+use App\Sponsor;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,8 +29,21 @@ class HomeController extends Controller
     public function index()
     {
         $miembros = Miembro::all();
+        $banners = Banner::all();
+        $causas = Causa::all();
+        $eventos = Evento::all();
+        $noticias = Noticia::all();
+        $proyectos = Proyecto::all();
+        $sponsors = Sponsor::all();
+
         return view('welcome', [
-            "miembros" => $miembros
+            "miembros" => $miembros,
+            "banners" => $banners,
+            "causas" => $causas,
+            "eventos" => $eventos,
+            "noticias" => $noticias,
+            "proyectos" => $proyectos,
+            "sponsors" => $sponsors
         ]);
     }
 }
