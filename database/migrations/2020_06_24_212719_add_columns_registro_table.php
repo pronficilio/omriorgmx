8943be4e5d7bd6b4f8a4b8fab9562f692e827d7e
GandyA23 <avila.gandy@gmail.com>
2020-06-24 21:56:55 -0500

    Migración: Añade columnas a Registro

---
+++ database/migrations/2020_06_24_212719_add_columns_registro_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsRegistroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::table('registro', function (Blueprint $table) {
+            $table->string('categoria')->nullable();
+            $table->string('usuario_omega')->nullable();
+            $table->string('password')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+        Schema::dropIfExists('registro');
+    }
+}
