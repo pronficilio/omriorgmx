7dba72a0bfc0c5b846922db426bb573fb0c4abcf
GandyA23 <avila.gandy@gmail.com>
2020-06-24 22:38:42 -0500

    Migración creada: CodeOrg

---
+++ database/migrations/2020_06_24_220952_create_codeorg_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCodeorgTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codeorg', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_registro');
+            $table->bigIncrements('leccion');
+            $table->timestamps();
+
+            $table->foreign('id_registro')->references('id')->on('registro');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codeorg');
+    }
+}
