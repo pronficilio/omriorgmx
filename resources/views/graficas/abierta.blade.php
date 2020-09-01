dfea802e7365f907fc676c0b43a7a25e6db2d529
GandyA23 <avila.gandy@gmail.com>
2020-09-01 14:04:43 -0500

    Se cambió la gráfica de las medallas

--- resources/views/graficas/abierta.blade.php
+++ resources/views/graficas/abierta.blade.php
@@ -11,30 +11,25 @@
         <div style="text-align: center;">
             <br><br><br><br><br>
             <h1>Medallas: Categoría Abierta</h1>
+            <br><br>
         </div>
 
         <div style="text-align: center;">
-            <br><br>
             <h3>Oro</h3>
-            <br>
         </div>
         
         <div id="oro"></div>
         <br>
 
         <div style="text-align: center;">
-            <br><br>
             <h3>Plata</h3>
-            <br>
         </div>
         
         <div id="plata"></div>
         <br>
 
         <div style="text-align: center;">
-            <br><br>
             <h3>Bronce</h3>
-            <br>
         </div>
         
         <div id="bronce"></div>
@@ -47,7 +42,7 @@
     <style>
         #oro, #plata, #bronce{
             width: auto;
-            height: 600px;
+            height: 400px;
         }
     </style>
 
@@ -55,6 +50,7 @@
     <script src="https://cdn.amcharts.com/lib/4/core.js"></script>
     <script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
     <script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
+    <script src="https://cdn.amcharts.com/lib/4/themes/kelly.js"></script>
 
     <!-- Chart code para medallas -->
     @foreach($colores as $color)
@@ -62,39 +58,44 @@
             am4core.ready(function() {
 
             // Themes begin
+            am4core.useTheme(am4themes_kelly);
             am4core.useTheme(am4themes_animated);
             // Themes end
 
-            var chart = am4core.create("{{$color}}", am4charts.PieChart3D);
-            chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+            // Create chart instance
+            var chart = am4core.create("{{$color}}", am4charts.PieChart);
             
             @php
-                $otros = 0 
+                $otros = 0 ;
+                $LIMITE = 4 ; 
             @endphp
 
+            //$LIMITE es para ver cuantas escuelas se van a mostrar + 1 de otros
+
+            // Add data
             @if($color == 'oro')
-                chart.data = [
-                    @foreach($oro as $medalla)
-                        @if($loop->index >= 9)
-                            @php
-                                $otros += $medalla->oro 
-                            @endphp
-                            @continue
-                        @endif
-
-                        { Escuelas: "{{$medalla->escuela}}", Medallas: {{$medalla->oro}} },
-                    @endforeach
-
-                    @if($otros > 0)
-                        { Escuelas: "Otras Escuelas", Medallas: {{$otros}} }
+            chart.data = [
+                @foreach($oro as $medalla)
+                    @if($loop->index >= $LIMITE)
+                        @php
+                            $otros += $medalla->oro 
+                        @endphp
+                        @continue
                     @endif
-                ];
+
+                    { Escuelas: "{{$medalla->escuela}}", Medallas: {{$medalla->oro}} },
+                @endforeach
+
+                @if($otros > 0)
+                    { Escuelas: "Otras Escuelas", Medallas: {{$otros}} }
+                @endif
+            ];
             @endif
 
             @if($color == 'plata')
                 chart.data = [
                     @foreach($plata as $medalla)
-                        @if($loop->index >= 9)
+                        @if($loop->index >= $LIMITE)
                             @php
                                 $otros += $medalla->plata 
                             @endphp
@@ -113,7 +114,7 @@
             @if($color == 'bronce')
                 chart.data = [
                     @foreach($bronce as $medalla)
-                        @if($loop->index >= 9)
+                        @if($loop->index >= $LIMITE)
                             @php
                                 $otros += $medalla->bronce 
                             @endphp
@@ -129,22 +130,32 @@
                 ];
             @endif
 
-            //Ancho de la grafica de medallas
-            chart.innerRadius = am4core.percent(40);
+            // Add and configure Series
+            var pieSeries = chart.series.push(new am4charts.PieSeries());
+            pieSeries.dataFields.value = "Medallas";
+            pieSeries.dataFields.category = "Escuelas";
+            pieSeries.innerRadius = am4core.percent(50);
+            pieSeries.ticks.template.disabled = true;
+            pieSeries.labels.template.disabled = true;
 
-            //Altura de la grafica de medallas
-            chart.depth = 50;
+            var rgm = new am4core.RadialGradientModifier();
+            rgm.brightnesses.push(-0.8, -0.8, -0.5, 0, - 0.5);
+            pieSeries.slices.template.fillModifier = rgm;
+            pieSeries.slices.template.strokeModifier = rgm;
+            pieSeries.slices.template.strokeOpacity = 0.4;
+            pieSeries.slices.template.strokeWidth = 0;
 
             chart.legend = new am4charts.Legend();
-
-            var series = chart.series.push(new am4charts.PieSeries3D());
-            series.dataFields.value = "Medallas";
-            series.dataFields.depthValue = "Medallas";
-            series.dataFields.category = "Escuelas";
-            series.slices.template.cornerRadius = 5;
-            series.colors.step = 3;
+            chart.legend.position = "right";
 
             }); // end am4core.ready()
-        </script>
+            </script>
     @endforeach
+
+
+
+<!-- Chart code -->
+<script>
+
+
 @endpush
