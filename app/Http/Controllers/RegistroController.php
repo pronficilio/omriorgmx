7bd1b7e0afe3acf006c1c71b0640b637171d7756
alexesr <alexisesr73c@gmail.com>
2021-03-18 22:40:10 -0600

    Enviar masivamente unicamente si no tienen pecado

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -337,7 +337,8 @@ class RegistroController extends Controller
      * (Esto solo se uso una vez,"el pecado 2021" fue porque aún no teníamos el registro individual)
      */
     public function registroMasivo(){
-        $usuarios = Registro::where('anio','=','2021')->get();
+        $usuarios = Registro::where([['anio','=','2021'],['pecado2021','=','0']])->get();
+        dd($usuarios);
         //$usuarios = Registro::where('email','=','alexisesr@outlook.com')->get();
         foreach($usuarios as $usuario){
             $params = "q=".$usuario->email;
